23a3e1bab5ee1e8dd28dd1d52a242f111ecde818
yaroslav zemlyanuhin <y.zemlyanuhin@cti.ru>
2014-11-15 09:00:14 +0300

    Commented hook for projectile. If project root recognized incorrectly

---
+++ personal/preload/projectile.el
@@ -0,0 +1,6 @@
+;(eval-after-load "projectile"
+;  '(progn
+;     (setq projectile-project-root-files-functions
+;           '(projectile-root-top-down
+;             projectile-root-top-down-recurring
+;             projectile-root-bottom-up))))
