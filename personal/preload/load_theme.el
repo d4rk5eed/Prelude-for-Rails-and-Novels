7238154029b380c3a695237b94effd28fda031f5
yaroslav zemlyanuhin <y.zemlyanuhin@cti.ru>
2014-11-21 19:18:15 +0300

    * Added utf-8 support  * Removed default encoding insert to head of rubies

--- personal/preload/load_theme.el
+++ personal/preload/load_theme.el
@@ -3,3 +3,10 @@
 (cua-mode t)
 (setq cua-auto-tabify-rectangles nil) ;; Don't tabify after rectangle commands
 (transient-mark-mode 1) ;; No region when it is not highlighted
+(set-language-environment 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-selection-coding-system 'utf-8)
+;; (set-locale-environment "en_GB.UTF-8")
+(prefer-coding-system 'utf-8)
+
+(setq ruby-insert-encoding-magic-comment nil)
